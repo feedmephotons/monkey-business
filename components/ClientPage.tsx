3c051ecb7ed2bafc343a7e543e7ed1b80740ae27
feedmephotons <folx2.0@gmail.com>
2026-07-22 03:41:36 -0400

    chore: slowed down hero carousel interval to 5.5s and updated contest flyer with massive prominent text

--- components/ClientPage.tsx
+++ components/ClientPage.tsx
@@ -132,7 +132,7 @@ export default function ClientPage({ posts, budget }: ClientPageProps) {
   useEffect(() => {
     const timer = setInterval(() => {
       setActiveFlyerIndex((prev) => (prev + 1) % FLYERS.length)
-    }, 3500) // Changes every 3.5 seconds
+    }, 5500) // Changes every 5.5 seconds
 
     return () => clearInterval(timer)
   }, [activeFlyerIndex])
